19ea4b941351de7ab93e18a407b1449b1f3c4858
Lina <125640650+LinaOne12@users.noreply.github.com>
2023-06-15 20:01:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -9,254 +9,276 @@ document.addEventListener("DOMContentLoaded", function () {
   const currentScore = document.getElementById("currentScore");
 
   const playerColorInput = document.getElementById("playerColorInput");
-  const obstacleColorInput = document.getElementById("obstacleColorInput");
   const resetPlayerColorBtn = document.getElementById("resetPlayerColorBtn");
-  const resetObstacleColorBtn = document.getElementById("resetObstacleColorBtn");
   const toggleCursorBtn = document.getElementById("toggleCursorBtn");
 
   const backgroundMusic = document.getElementById("backgroundMusic");
 
-  const player = {
-    x: canvas.width / 2 - 25,
-    y: canvas.height - 50,
-    width: 50,
-    height: 50,
-    color: playerColorInput.value,
-    speed: 6,
-    direction: 0,
-  };
-
-  let gameStarted = false;
-  let gamePaused = false;
-  let round = 0;
-  let score = 0;
-  let obstacles = [];
-  let animationId;
-
-  function updatePlayer() {
-    player.x += player.direction * player.speed;
-
-    if (player.x < 0) {
-      player.x = 0;
-    } else if (player.x + player.width > canvas.width) {
-      player.x = canvas.width - player.width;
-    }
-  }
-
-  function drawPlayer() {
-    ctx.fillStyle = player.color;
-    ctx.fillRect(player.x, player.y, player.width, player.height);
-  }
-
-  function updateObstacles() {
-    if (Math.random() < 0.02 + round / 1000) {
-      const obstacle = {
-        x: Math.random() * (canvas.width - 50),
-        y: 0,
-        width: 50,
-        height: 50,
-        color: obstacleColorInput.value,
-        speed: 3 + round / 100,
-      };
-      obstacles.push(obstacle);
-    }
-
-    for (let i = 0; i < obstacles.length; i++) {
-      obstacles[i].y += obstacles[i].speed;
-
-      if (detectCollision(player, obstacles[i])) {
-        stopGame();
-        showGameOver();
-        return;
-      }
-
-      if (obstacles[i].y > canvas.height) {
-        obstacles.splice(i, 1);
-        i--;
-        score++;
-        currentScore.textContent = "Score: " + score;
-      }
-    }
-  }
-
-  function drawObstacles() {
-    for (let i = 0; i < obstacles.length; i++) {
-      ctx.fillStyle = obstacles[i].color;
-      ctx.fillRect(
-        obstacles[i].x,
-        obstacles[i].y,
-        obstacles[i].width,
-        obstacles[i].height
-      );
-    }
-  }
-
-  function detectCollision(object1, object2) {
-    return (
-      object1.x < object2.x + object2.width &&
-      object1.x + object1.width > object2.x &&
-      object1.y < object2.y + object2.height &&
-      object1.y + object1.height > object2.y
-    );
-  }
-
-  function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-
-  function draw() {
-    clearCanvas();
-    drawPlayer();
-    drawObstacles();
-
-    if (!gamePaused) {
-      updatePlayer();
-      updateObstacles();
-    }
-
-    if (gameStarted) {
-      animationId = requestAnimationFrame(draw);
-    }
-  }
-
-  function startGame() {
-    if (!gameStarted) {
-      gameStarted = true;
-      gamePaused = false;
-      round = 0;
-      score = 0;
-      obstacles = [];
-      currentRound.textContent = "Round: " + round;
-      currentScore.textContent = "Score: " + score;
-      startButton.disabled = true;
-      pauseButton.disabled = false;
-      resetButton.disabled = true;
-
-      backgroundMusic.currentTime = 0; // Reset music to the beginning
-      backgroundMusic.play();
-      draw();
-    }
-  }
-
+  // Background color 
   canvas.style.backgroundColor = "#313131";
 
-  function pauseGame() {
-    if (gameStarted && !gamePaused) {
-      gamePaused = true;
-      pauseButton.textContent = "Resume";
+  const game = {
+    canvas: canvas,
+    ctx: ctx,
+    player: {
+      x: canvas.width / 2 - 25,
+      y: canvas.height - 50,
+      width: 50,
+      height: 50,
+      color: playerColorInput.value,
+      speed: 8,
+      direction: 0,
+    },
+    round: 0,
+    score: 0,
+    obstacles: [],
+    gameStarted: false,
+    gamePaused: false,
+    animationId: null,
+
+    startGame() {
+      if (!this.gameStarted) {
+        this.gameStarted = true;
+        this.gamePaused = false;
+        this.round = 0;
+        this.score = 0;
+        this.obstacles = [];
+        currentRound.textContent = "Round: " + this.round;
+        currentScore.textContent = "Score: " + this.score;
+        startButton.disabled = true;
+        pauseButton.disabled = false;
+        resetButton.disabled = true;
+
+        backgroundMusic.currentTime = 0;
+        backgroundMusic.play();
+        this.draw();
+      }
+    },
+
+    pauseGame() {
+      if (this.gameStarted && !this.gamePaused) {
+        this.gamePaused = true;
+        pauseButton.textContent = "Resume (R)";
+        backgroundMusic.pause();
+        cancelAnimationFrame(this.animationId);
+        this.showGamePaused();
+      } else if (this.gameStarted && this.gamePaused) {
+        this.gamePaused = false;
+        pauseButton.textContent = "Pause (R)";
+        backgroundMusic.play();
+        this.hideGamePaused();
+        this.draw();
+      }
+    },
+
+    stopGame() {
+      this.gameStarted = false;
+      this.gamePaused = false;
+      startButton.disabled = false;
+      pauseButton.disabled = true;
+      resetButton.disabled = false;
       backgroundMusic.pause();
-      cancelAnimationFrame(animationId);
-      showGamePaused(); // Display "Game Paused" message
-    } else if (gameStarted && gamePaused) {
-      gamePaused = false;
-      pauseButton.textContent = "Pause";
-      backgroundMusic.play();
-      hideGamePaused(); // Hide "Game Paused" message
-      draw();
-    }
-  }
-  
-  function showGamePaused() {
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0, canvas.height / 2 - 30, canvas.width, 60);
-    ctx.fillStyle = "#FFF";
-    ctx.font = "30px Arial";
-    ctx.fillText("Game Paused!", canvas.width / 2 - 95, canvas.height / 2 + 10);
-  }
-  
-  
-  function hideGamePaused() {
-    clearCanvas();
-  }
-  
+      cancelAnimationFrame(this.animationId);
+    },
 
-  function stopGame() {
-    gameStarted = false;
-    gamePaused = false;
-    startButton.disabled = false;
-    pauseButton.disabled = true;
-    resetButton.disabled = false;
-    backgroundMusic.pause();
-    cancelAnimationFrame(animationId);
-  }
+    resetGame() {
+      this.gameStarted = false;
+      this.gamePaused = false;
+      startButton.disabled = false;
+      pauseButton.disabled = true;
+      resetButton.disabled = true;
+      backgroundMusic.pause();
+      cancelAnimationFrame(this.animationId);
+      this.clearCanvas();
+      this.player.color = playerColorInput.value;
+      this.obstacles = [];
+      currentRound.textContent = "Round: 0";
+      currentScore.textContent = "Score: 0";
+    },
 
-  function showGameOver() {
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0, canvas.height / 2 - 30, canvas.width, 60);
-    ctx.fillStyle = "#FFF";
-    ctx.font = "30px Arial";
-    ctx.fillText("Game Over!", canvas.width / 2 - 80, canvas.height / 2 + 10);
-  }
+    updatePlayer() {
+      if (this.player.direction === "left") {
+        this.player.x -= this.player.speed;
+      } else if (this.player.direction === "right") {
+        this.player.x += this.player.speed;
+      }
 
-  function resetGame() {
-    stopGame();
-    clearCanvas();
-    player.color = playerColorInput.value;
-    obstacles = [];
-    currentRound.textContent = "Round: 0";
-    currentScore.textContent = "Score: 0";
-  }
+      if (this.player.x < 0) {
+        this.player.x = 0;
+      } else if (this.player.x + this.player.width > this.canvas.width) {
+        this.player.x = this.canvas.width - this.player.width;
+      }
+    },
 
-  function applySettings() {
-    player.color = playerColorInput.value;
-    backgroundMusic.volume = difficulty.value === "easy" ? 0.2 : 0.5;
-  }
+    drawPlayer() {
+      ctx.fillStyle = this.player.color;
+      ctx.fillRect(
+        this.player.x,
+        this.player.y,
+        this.player.width,
+        this.player.height
+      );
+    },
 
-  function resetPlayerColor() {
-    playerColorInput.value = "#00D5FF";
-    player.color = "#00D5FF";
-  }
+    updateObstacles() {
+      if (Math.random() < 0.05 + this.round / 500) {
+        const obstacle = {
+          x: Math.random() * (this.canvas.width - 50),
+          y: -50,
+          width: 50,
+          height: 50,
+          color: this.getRandomBrightColor(),
+          speed: 3 + this.round / 100,
+        };
+        this.obstacles.push(obstacle);
+      }
 
-  function resetObstacleColor() {
-    obstacleColorInput.value = "#FF0000";
-  }
+      for (let i = 0; i < this.obstacles.length; i++) {
+        this.obstacles[i].y += this.obstacles[i].speed;
 
-  function toggleCursorVisibility() {
-    var bodyElement = document.getElementsByTagName("body")[0];
-    bodyElement.style.cursor = bodyElement.style.cursor === "none" ? "default" : "none";
-  }
+        if (this.detectCollision(this.player, this.obstacles[i])) {
+          this.stopGame();
+          this.showGameOver();
+          return;
+        }
 
-  function handleKeydown(event) {
-    if (event.key === "ArrowLeft" || event.key === "Left") {
-      player.direction = -1;
-    } else if (event.key === "ArrowRight" || event.key === "Right") {
-      player.direction = 1;
-    } else if (event.key === "q" || event.key === "Q") {
-      toggleCursorVisibility();
-    } else if (event.key === "e" || event.key === "E") {
-      startGame();
+        if (this.obstacles[i].y > this.canvas.height) {
+          this.obstacles.splice(i, 1);
+          i--;
+          this.score++;
+          currentScore.textContent = "Score: " + this.score;
+
+          if (this.score % 10 === 0) {
+            this.round++;
+            currentRound.textContent = "Round: " + this.round;
+          }
+        }
+      }
+    },
+
+    drawObstacles() {
+      for (let i = 0; i < this.obstacles.length; i++) {
+        ctx.fillStyle = this.obstacles[i].color;
+        ctx.fillRect(
+          this.obstacles[i].x,
+          this.obstacles[i].y,
+          this.obstacles[i].width,
+          this.obstacles[i].height
+        );
+      }
+    },
+
+    detectCollision(object1, object2) {
+      return (
+        object1.x < object2.x + object2.width &&
+        object1.x + object1.width > object2.x &&
+        object1.y < object2.y + object2.height &&
+        object1.y + object1.height > object2.y
+      );
+    },
+
+    clearCanvas() {
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+
+    draw() {
+      this.clearCanvas();
+      this.drawPlayer();
+      this.drawObstacles();
+
+      if (!this.gamePaused) {
+        this.updatePlayer();
+        this.updateObstacles();
+      }
+
+      if (this.gameStarted) {
+        this.animationId = requestAnimationFrame(() => this.draw());
+      }
+    },
+
+    showGamePaused() {
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, this.canvas.height / 2 - 30, this.canvas.width, 60);
+      ctx.fillStyle = "#FFF";
+      ctx.font = "30px Arial";
+      ctx.fillText(
+        "Game Paused!",
+        this.canvas.width / 2 - 95,
+        this.canvas.height / 2 + 10
+      );
+    },
+
+    hideGamePaused() {
+      this.clearCanvas();
+    },
+
+    showGameOver() {
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, this.canvas.height / 2 - 30, this.canvas.width, 60);
+      ctx.fillStyle = "#FFF";
+      ctx.font = "30px Arial";
+      ctx.fillText(
+        "Game Over!",
+        this.canvas.width / 2 - 80,
+        this.canvas.height / 2 + 10
+      );
+    },
+
+    applySettings() {
+      this.player.color = playerColorInput.value;
+      backgroundMusic.volume = volumeSlider.value;
+    },
+
+    resetPlayerColor() {
+      playerColorInput.value = "#00D5FF";
+      this.player.color = "#00D5FF";
+    },
+
+    toggleCursorVisibility() {
+      const bodyElement = document.getElementsByTagName("body")[0];
+      bodyElement.style.cursor =
+        bodyElement.style.cursor === "none" ? "default" : "none";
+    },
+
+    getRandomBrightColor() {
+      const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
+      const randomIndex = Math.floor(Math.random() * colors.length);
+      return colors[randomIndex];
+    },
+  };
+
+  startButton.addEventListener("click", game.startGame.bind(game));
+  pauseButton.addEventListener("click", game.pauseGame.bind(game));
+  resetButton.addEventListener("click", game.resetGame.bind(game));
+  playerColorInput.addEventListener("change", game.applySettings.bind(game));
+  resetPlayerColorBtn.addEventListener(
+    "click",
+    game.resetPlayerColor.bind(game)
+  );
+  toggleCursorBtn.addEventListener(
+    "click",
+    game.toggleCursorVisibility.bind(game)
+  );
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "e" || event.key === "E") {
+      game.startGame();
     } else if (event.key === "r" || event.key === "R") {
-      pauseGame();
+      game.pauseGame();
     } else if (event.key === "t" || event.key === "T") {
-      resetGame();
+      game.resetGame();
     } else if (event.key === "y" || event.key === "Y") {
-      toggleCursorVisibility();
+      game.toggleCursorVisibility();
+    } else if (event.key === "ArrowLeft") {
+      game.player.direction = "left";
+    } else if (event.key === "ArrowRight") {
+      game.player.direction = "right";
     }
-  }
-
-
-  function handleKeyup(event) {
-    if (
-      (event.key === "ArrowLeft" || event.key === "Left") &&
-      player.direction === -1
-    ) {
-      player.direction = 0;
-    } else if (
-      (event.key === "ArrowRight" || event.key === "Right") &&
-      player.direction === 1
-    ) {
-      player.direction = 0;
+  });
+  
+  document.addEventListener("keyup", function (event) {
+    if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+      game.player.direction = 0;
     }
-  }
-
-  startButton.addEventListener("click", startGame);
-  pauseButton.addEventListener("click", pauseGame);
-  resetButton.addEventListener("click", resetGame);
-  playerColorInput.addEventListener("input", applySettings);
-  obstacleColorInput.addEventListener("input", applySettings);
-  resetPlayerColorBtn.addEventListener("click", resetPlayerColor);
-  resetObstacleColorBtn.addEventListener("click", resetObstacleColor);
-  toggleCursorBtn.addEventListener("click", toggleCursorVisibility);
-  document.addEventListener("keydown", handleKeydown);
-  document.addEventListener("keyup", handleKeyup);
+  });
 });
