8372c61ba109b447f5ac56c989ea190268f4d984
Lina <125640650+LinaOne12@users.noreply.github.com>
2023-07-21 00:59:46 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 const navLinks = document.querySelectorAll('.nav-link');
-const subMenus = document.querySelectorAll('li ul');
+  const subMenus = document.querySelectorAll('li ul');
 
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    // Check if the link has a sub-menu
-    const subMenu = link.nextElementSibling;
-    if (subMenu && window.innerWidth <= 768) {
-      // Toggle the sub-menu's display
-      subMenu.classList.toggle('show');
-      // Hide other open sub-menus
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      // Check if the link has a sub-menu
+      const subMenu = link.nextElementSibling;
+      if (subMenu && window.innerWidth <= 768) {
+        // Toggle the sub-menu's display
+        subMenu.classList.toggle('show');
+        // Hide other open sub-menus
+        subMenus.forEach(menu => {
+          if (menu !== subMenu && menu.classList.contains('show')) {
+            menu.classList.remove('show');
+          }
+        });
+      }
+    });
+  });
+
+  // Close the mobile navigation when clicking outside the menu or the link
+  document.addEventListener('click', (event) => {
+    const nav = document.querySelector('nav');
+    const clickedElement = event.target;
+
+    if (!nav.contains(clickedElement) && window.innerWidth <= 768) {
       subMenus.forEach(menu => {
-        if (menu !== subMenu && menu.classList.contains('show')) {
+        if (menu.classList.contains('show')) {
           menu.classList.remove('show');
         }
       });
+    } else if (clickedElement.classList.contains('nav-link') && window.innerWidth <= 768) {
+      // If the clicked element is a nav link with a sub-menu, toggle its display
+      const subMenu = clickedElement.nextElementSibling;
+      if (subMenu) {
+        subMenu.classList.toggle('show');
+      }
     }
   });
-});
-
-// Close the mobile navigation when clicking outside the menu
-document.addEventListener('click', (event) => {
-  const nav = document.querySelector('nav');
-  if (!nav.contains(event.target) && window.innerWidth <= 768) {
-    subMenus.forEach(menu => {
-      if (menu.classList.contains('show')) {
-        menu.classList.remove('show');
-      }
-    });
-  }
-});
 
 
 // Function to open the image preview modal
